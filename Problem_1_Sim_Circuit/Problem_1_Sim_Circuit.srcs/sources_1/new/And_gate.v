7cd05bac12fc0344cb2f40fce90233514905bfad
ranasiddu18 <markasiddu18@gmail.com>
2026-04-08 22:56:06 +0530

    1st Problem

---
+++ Problem_1_Sim_Circuit/Problem_1_Sim_Circuit.srcs/sources_1/new/And_gate.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.04.2026 22:50:49
+// Design Name: 
+// Module Name: And_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module And_gate(
+input a,
+    input b,
+    output q );
+
+    assign q = a&b;
+ 
+endmodule

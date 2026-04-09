f3298d091923ce7bdfa74a4ffc18b2970c9ed49e
ranasiddu18 <markasiddu18@gmail.com>
2026-04-09 22:15:18 +0530

    Problem 2

---
+++ Problem_2/Problem_2.srcs/sources_1/new/top_module.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.04.2026 22:12:08
+// Design Name: 
+// Module Name: top_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output q
+    );
+    assign q = ~(a^b^c^d);
+endmodule

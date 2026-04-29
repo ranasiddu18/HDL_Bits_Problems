27fc7daea9a8675efe5094583da1a3f8a10a6e13
ranasiddu18 <markasiddu18@gmail.com>
2026-04-29 16:59:38 +0530

    Problem 19

---
+++ Problem_19_Kmap3/Problem_19_Kmap3.srcs/sources_1/new/top_module.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 29.04.2026 16:57:21
+// Design Name: 
+// Module Name: top_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_module(
+ input a,
+    input b,
+    input c,
+    input d,
+    output out  ); 
+  assign out = a | (~b & c);
+endmodule

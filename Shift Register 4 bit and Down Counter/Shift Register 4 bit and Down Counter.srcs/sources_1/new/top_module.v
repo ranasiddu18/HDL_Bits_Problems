13b57b5c14e434cdf11f2b349cf0b9ad5cad8778
ranasiddu18 <markasiddu18@gmail.com>
2026-07-26 22:21:04 +0530

    Shift register and Down counter

---
+++ Shift Register 4 bit and Down Counter/Shift Register 4 bit and Down Counter.srcs/sources_1/new/top_module.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 26.07.2026 22:12:18
+// Design Name: 
+// Module Name: top_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_module (
+    input clk,
+    input shift_ena,
+    input count_ena,
+    input data,
+    output reg [3:0] q);
+    
+    always@(posedge clk) begin
+        
+        if(shift_ena)
+        q<={q[3:1],data};
+        else if(count_ena && q==0 )
+        q <= q-1;
+    end 
+        
+endmodule

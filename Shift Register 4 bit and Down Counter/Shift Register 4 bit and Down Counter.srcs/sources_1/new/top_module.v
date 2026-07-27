b4f2793dcada8ad403ee84dc43a492b3cc428b65
ranasiddu18 <markasiddu18@gmail.com>
2026-07-27 20:54:18 +0530

    Changes in code

--- Shift Register 4 bit and Down Counter/Shift Register 4 bit and Down Counter.srcs/sources_1/new/top_module.v
+++ Shift Register 4 bit and Down Counter/Shift Register 4 bit and Down Counter.srcs/sources_1/new/top_module.v
@@ -28,11 +28,12 @@ module top_module (
     output reg [3:0] q);
     
     always@(posedge clk) begin
-        
-        if(shift_ena)
-        q<={q[3:1],data};
-        else if(count_ena && q==0 )
-        q <= q-1;
+        if(shift_ena) begin
+            q <= {q[2:0],data}; end
+        else if(count_ena && q!=0 )
+            q <= q-1;
+        else if( count_ena && q==0)
+            q <= 4'hf;
     end 
         
 endmodule

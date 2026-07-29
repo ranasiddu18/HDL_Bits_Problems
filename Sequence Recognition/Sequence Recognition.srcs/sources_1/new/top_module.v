d117afa5ff5508dbe3c5b4b555a7932c3b179fa9
ranasiddu18 <markasiddu18@gmail.com>
2026-07-29 21:52:42 +0530

    Sequence Recognition

---
+++ Sequence Recognition/Sequence Recognition.srcs/sources_1/new/top_module.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 29.07.2026 21:51:38
+// Design Name: 
+// Module Name: top_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_module(
+
+    );
+endmodule

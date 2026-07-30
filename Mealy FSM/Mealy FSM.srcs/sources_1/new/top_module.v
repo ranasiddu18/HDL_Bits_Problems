6cf86888b228d4477ee0f4b8c207777617300358
ranasiddu18 <markasiddu18@gmail.com>
2026-07-30 22:26:25 +0530

    Mealy FSM

---
+++ Mealy FSM/Mealy FSM.srcs/sources_1/new/top_module.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.07.2026 22:24:39
+// Design Name: 
+// Module Name: top_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_module(
+
+    );
+endmodule
